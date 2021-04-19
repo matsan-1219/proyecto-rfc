556e5260dad09e1e05de4823f6f83c5f10a78fbd
sandra <sandy_9011@live.com.mx>
2021-04-19 13:45:20 -0500

    version 1

---
+++ rfc2.ts
@@ -0,0 +1,73 @@
+//creamos una cadena llamada apellido
+//el String lo usaremos para manipular la cadena apellidop
+//vamos a concatenar el apellido para que lo volvamos a mayuscula y
+//con charArt solo me devolvera uns letra del apellido
+var apellidop =new String("avila");
+apellidop =apellidop.toUpperCase().charAt(0);
+console.log(apellidop);
+
+
+//en este caso seria lo mismo solo em charAt cambiaremos la posicion que nos tiene que mostrar
+//creamos una cadena llamada apellido
+//el String lo usaremos para manipular la cadena apellidop
+//vamos a concatenar el apellido para que lo volvamos a mayuscula y
+//con charArt solo me devolvera uns letra del apellido
+var apellidop =new String("avila");
+apellidop =apellidop.toUpperCase().charAt(2);
+console.log(apellidop);
+
+//creamos una cadena llamada apellidom
+//el String lo usaremos para manipular la cadena (gonzalez)
+//vamos a concatenar el apellido para que lo volvamos a mayuscula y
+//con charArt solo me devolvera uns letra del apellidom
+var apellidom =new String("gonzalez");
+apellidom = apellidom.charAt(0).toUpperCase();
+console.log(apellidom);
+
+//creamos una cadena llamada nombre
+//el String lo usaremos para manipular la cadena (sandra)
+//vamos a concatenar el nombre para que lo volvamos a mayuscula con toUpperCase() y
+//con charArt solo me devolvera la primer posicion de la letra del nombre
+
+var nombre =new String("sandra");
+nombre =nombre.charAt(0).toUpperCase();
+console.log(nombre);
+
+
+//agregamos el dia e igul forma creamos la cadena 
+//creamos una cadena llamada dia
+//el String lo usaremos para manipular la cadena (dia)
+//vamos a concatenar el dia para que
+//con charArt y slice solo me devolvera el primer numero
+var dia=new String("12");
+dia=dia.slice(0).charAt(0);;
+console.log(dia);
+
+var dia=new String("12");
+dia=dia.slice(1).charAt(0);
+console.log(dia);
+//agregamos el dia e igul forma creamos la cadena 
+//creamos una cadena llamada mes
+//el String lo usaremos para manipular la cadena (mes)
+//vamos a concatenar el mes para  que
+//con charArt y slice solo me devolvera el primer numero
+var mes=new String("05");
+mes=mes.slice(0).charAt(0);
+console.log(mes);
+
+var mes=new String("05");
+mes=mes.slice(1).charAt(0);
+console.log(mes);
+//agregamos el dia e igul forma creamos la cadena 
+//creamos una cadena llamada año
+//el String lo usaremos para manipular la cadena (año)
+//vamos a concatenar el año para  que
+//con charArt y slice solo me devolvera el primer numero
+var año=new String("1996");
+año=año.slice(2).charAt(0);
+console.log(año);
+var año=new String("1996");
+año=año.slice(3).charAt(0);
+console.log(año);
+
+
